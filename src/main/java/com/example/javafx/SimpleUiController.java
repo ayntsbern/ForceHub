9d4eeab31310481b5f5bb2568ba49cb61ebe45a5
Yulia Yakushenkova <yuliya.yakushenkova@customertimes.com>
2020-06-04 18:39:21 +0300

    Initial commit

---
+++ src/main/java/com/example/javafx/SimpleUiController.java
@@ -0,0 +1,24 @@
+package com.example.javafx;
+
+import javafx.application.HostServices;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleUiController {
+
+    private final HostServices hostServices;
+
+    @FXML public Button button;
+    @FXML public Label label;
+
+    SimpleUiController(HostServices hostServices) {
+        this.hostServices = hostServices;
+    }
+
+    @FXML public void initilize() {
+        this.button.setOnAction(actionEvent -> this.label.setText(this.hostServices.getDocumentBase()));
+    }
+}
